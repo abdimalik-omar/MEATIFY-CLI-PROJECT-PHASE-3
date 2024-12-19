1bda95adc4cd533986b65259ee70b37383d7115a
abdimalik-omar <abdimalik.omar1@student.moringaschool.com>
2024-12-19 21:06:43 +0300

    modified models

--- meatify/models.py
+++ meatify/models.py
@@ -1,28 +1,5 @@
-# from sqlalchemy import Column, Integer, String, create_engine
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker
-
-# Base = declarative_base()
-
-# class User(Base):
-#     __tablename__ = 'users'
-
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String, nullable=False)
-#     email = Column(String, nullable=False)
-
-# # Create an engine that stores data in the local SQLite database
-# engine = create_engine('sqlite:///butchery.db')
-
-# # Create all tables defined by Base (including User)
-# Base.metadata.create_all(engine)
-
-# # Session configuration
-# Session = sessionmaker(bind=engine)
-# session = Session()
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, declarative_base
 
 Base = declarative_base()
 
